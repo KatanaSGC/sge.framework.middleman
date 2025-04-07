23c809618c8921906f0a86cfb1b32d3b1dfb2ea0
Agustin Gonzalez <agm.sge@outlook.com>
2025-04-07 08:05:57 -0600

    add license

--- src/obj/Debug/net8.0/sge.framework.middleman.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/sge.framework.middleman.AssemblyInfo.cs
@@ -10,13 +10,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("sge.framework.middleman")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KatanaSGC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("This package is an implementation of the mediator pattern.")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1+39432f1f55346012a196e0a268c43c873e678066")]
 [assembly: System.Reflection.AssemblyProductAttribute("sge.framework.middleman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sge.framework.middleman")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/KatanaSGC/sge.framework.middleman")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
